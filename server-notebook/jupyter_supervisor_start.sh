3431689fd174e1a5bc90dcb008f8a9c3bae6d889
Errol Lloyd <errolkjlloyd@gmail.com>
2020-05-07 21:17:43 +1000

    Overwrite CMD with script that includes supervisord

---
+++ server-notebook/jupyter_supervisor_start.sh
@@ -0,0 +1,7 @@
+# Start the supervisor with specified conf path
+supervisord -c /etc/supervisor/supervisord.conf
+
+# Originally intended entry point - jupyter bash script for starting the notebook
+start-notebook.sh
+
+

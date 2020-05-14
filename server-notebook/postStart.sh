60ccabf194b16aa1f20831e9d084ea016673dd92
Henry Walshaw <henry.walshaw@gmail.com>
2020-05-14 11:05:09 +1000

    Update the docker image with additional requirements
    1. Move Python pip installs to the last section to help with caching
    2. Add apt installs
    3. Add conda installs
    4. Update requirements
    5. Modify script to setup keyring environment for headless mode

--- server-notebook/postStart.sh
+++ server-notebook/postStart.sh
@@ -1,20 +1,24 @@
-
 cp /tmp/.watchdog_tricks.yml ./
 
 # Check if symlinks exist, if not, make them
 # Avoids errors for each restart of server with persistent volume as home
 # NOTE postStart hook runs in mounted/persistent home directory
 if [ ! -L ./Data ]; then
-	ln -s /home/data/Data ./Data
+        ln -s /home/data/Data ./Data
 fi
 
 if [ ! -L ./trainer ]; then
-	ln -s /home/shared/trainer ./trainer
+        ln -s /home/shared/trainer ./trainer
 fi
 
 if [ ! -L ./_Materials ]; then
-	ln -s /home/data/_Materials ./_Materials
+        ln -s /home/data/_Materials ./_Materials
 fi
+
+# make the location for keyringrc.cfg
+mkdir -p ~/.local/share/python_keyring/
+cp -f /tmp/keyringrc.cfg .local/share/python_keyring/keyringrc.cfg
+
 
 # last, as will return exit code "1" when supervisord already running
 supervisord -c /etc/supervisor/supervisord.conf

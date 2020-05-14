7b80eedfe56f3c982903b57194c0a6fd27499c92
Errol Lloyd <errolkjlloyd@gmail.com>
2020-05-14 10:30:13 +1000

    clean up postStart redundancies

--- server-notebook/jupyter_supervisor_start.sh
+++ server-notebook/jupyter_supervisor_start.sh
@@ -1,3 +1,6 @@
+# Now redundant with the postStart.sh
+# Rely instead on the originally defined CMD ["start-notebook.sh"] in the base-notebook
+
 # Start the supervisor with specified conf path
 supervisord -c /etc/supervisor/supervisord.conf
 

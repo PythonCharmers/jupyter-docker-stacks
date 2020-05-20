05a88b8ecc1777df6814f567e5aafbf3d1a4d689
Henry Walshaw <henry.walshaw@gmail.com>
2020-05-20 12:34:37 +1000

    Under the assumption that the PVC will be mounted to /home/private
    Fixes to setup the server to use the shared drive as the folder for
    users to work from, with the private files available if needed.
    
    Also adds a source to /home/shared to guarantee that we can modify
    postStart without constantly rebuilding the Docker image

--- server-notebook/postStart.sh
+++ server-notebook/postStart.sh
@@ -1,33 +1,52 @@
-cp /tmp/.watchdog_tricks.yml ./
+# Set up the users home directory on the shared drive
+mkdir -p /home/shared/${JUPYTERHUB_USER}
+fix-permissions /home/shared/${JUPYTERHUB_USER}
+
+mount -o bind /home/shared/${JUPYTERHUB_USER} /home/jovyan
+fix-permissions /home/jovyan
+
 
 # Check if symlinks exist, if not, make them
 # Avoids errors for each restart of server with persistent volume as home
 # NOTE postStart hook runs in mounted/persistent home directory
+## >> Data
 if [ ! -L ./Data ]; then
         ln -s /home/data/Data ./Data
 fi
+if [ ! -L /Data ]; then
+        ln -s /home/data/Data /Data
+fi
 
+## >> Materials
+if [ ! -L ./Materials ]; then
+        ln -s /home/data/Materials ./Materials
+fi
+if [ ! -L /Materials ]; then
+        ln -s /home/data/Materials /Materials
+fi
+
+## >> Trainer
 if [ ! -L ./Trainer ]; then
         ln -s /home/shared/trainer ./Trainer
 fi
-
-if [ ! -L ./Materials ]; then
-        ln -s /home/data/_Materials ./Materials
+if [ ! -L /Trainer ]; then
+        ln -s /home/shared/trainer /Trainer
 fi
 
-# make the location for keyringrc.cfg
+
+# > make the location for keyringrc.cfg
 mkdir -p ~/.local/share/python_keyring/
 cp -f /tmp/keyringrc.cfg .local/share/python_keyring/keyringrc.cfg
 
-# copy over simple bashrc file for terminal usage
-cp -f /tmp/.bashrc ./
+# > copy over simple bashrc file for terminal usage
+cp -f /tmp/.bashrc ./.profile
 
-# remove lost+found folder
-rm -Rf $NB_USER/lost+found
+# > watchdog
 
-# make Data and _Materials read-only (easy to bypass but prevents
-# accidental modifications)
-# chmod u-w $NB_USER/data/Data $NB_USER/data/_Materials
+cp /tmp/.watchdog_tricks.yml ./   # everyone to run their own conversions
 
-# last, as will return exit code "1" when supervisord already running
-supervisord -c /etc/supervisor/supervisord.conf
+# > extras from /home/shared/postStart.sh
+if [ -f /home/shared/postStart.sh ]
+   source /home/shared/postStart.sh
+   echo "ran shared poststart from ${JUPYTERHUB_USER}" >> /home/shared/start_logs.log
+fi

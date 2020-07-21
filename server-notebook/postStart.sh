eb97183c4cd2452d4acb56ff6240ac880e948f7e
Henry Walshaw <henry.walshaw@gmail.com>
2020-07-21 15:46:18 +1000

    Only thing docker poststart does is call poststart in /home/shared

--- server-notebook/postStart.sh
+++ server-notebook/postStart.sh
@@ -1,22 +1,3 @@
-# Check if symlinks exist, if not, make them
-# Avoids errors for each restart of server with persistent volume as home
-# NOTE postStart hook runs in mounted/persistent home directory
-## >> Data
-if [ ! -L ./Data ]; then
-        ln -s /home/data/Data ./Data
-fi
-
-# > make the location for keyringrc.cfg
-mkdir -p ~/.local/share/python_keyring/
-cp -f /tmp/keyringrc.cfg .local/share/python_keyring/keyringrc.cfg
-
-# > copy over simple bashrc file for terminal usage
-cp -f /tmp/.bashrc ./.profile
-
-# > watchdog
-
-cp /tmp/.watchdog_tricks.yml ./   # everyone to run their own conversions
-
 # > extras from /home/shared/postStart.sh
 if [ -f /home/shared/postStart.sh ]; then
     {
